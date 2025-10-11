0feb9644d602c2250d5dcc25432d9cb053efcb8e
taniachris8 <dove6464dove@gmail.com>
2025-10-11 14:06:05 +0300

    added licenses.txt

---
+++ src/js/__tests__/cardBrandAffiliation.test.js
@@ -0,0 +1,6 @@
+import { CardFormWidget } from "../widget";
+import { detectBrand } from "../cardBrandAffiliation";
+
+describe("testing detectBrand function", () => { 
+    
+})
---
+++ src/js/__tests__/cardNumberValidator.test.js
@@ -0,0 +1,4 @@
+import { CardFormWidget } from "../widget";
+import { isValid } from "../cardNumberValidator";
+
+describe("testing isValid function", () => {});
